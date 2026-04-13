a2841d63abaa4efc09e1a84b8a36de5c23d63913
이주현 <juhyun0829@gmail.com>
2026-04-13 16:35:40 +0900

    Java

---
+++ backend/java-practice/src/sec02/chap02/Main.java
@@ -0,0 +1,15 @@
+package sec02.chap02;
+
+public class Main {
+    static void main() {
+        System.out.println("Hello world!");
+
+        // 한줄 주석
+
+        /*
+        여러줄 주석
+         */
+
+        System.out.println("헬로 월드!");
+    }
+}
