130e1dba49a38bfc3fa6c35fa916229b395e3477
이주현 <juhyun0829@gmail.com>
2026-07-07 11:07:50 +0900

    action

--- frontend/react-notes/app12-react19/src/App.jsx
+++ frontend/react-notes/app12-react19/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Use from "./components/use/Use";
+import Action from "./components/action/Action";
 
 function App() {
-  return <Use />;
+  return (
+    // <Use />
+    <Action />
+  );
 }
 
 export default App;
--- frontend/react-notes/app12-react19/src/components/action/Action.jsx
+++ frontend/react-notes/app12-react19/src/components/action/Action.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+const PostForm = ({ addPost }) => {
+  const formAction = async (formData) => {
+    const newPost = {
+      title: formData.get("title"),
+    };
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    addPost(newPost);
+  };
+
+  // action
+  return (
+    <form action={formAction}>
+      <input type="text" name="title" />
+      <button type="submit">submit</button>
+    </form>
+  );
+};
 
 const Action = () => {
-  return <div>Action</div>;
+  const [posts, setPosts] = useState([]);
+  const addPost = (newPost) => {
+    setPosts((posts) => [...posts, newPost]);
+  };
+
+  return (
+    <div>
+      <PostForm addPost={addPost} />
+      {console.log(posts)}
+      {posts.map((post, index) => {
+        return <h2 key={index}>{post.title}</h2>;
+      })}
+    </div>
+  );
 };
 
 export default Action;
