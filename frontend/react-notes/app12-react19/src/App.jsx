5565c5ca000c66f81aafa6f97df8e3c18a09d860
이주현 <juhyun0829@gmail.com>
2026-07-07 12:15:09 +0900

    useOptimistic

--- frontend/react-notes/app12-react19/src/App.jsx
+++ frontend/react-notes/app12-react19/src/App.jsx
@@ -4,13 +4,15 @@ import Use from "./components/use/Use";
 import Action from "./components/action/Action";
 import UseFormStatus from "./components/useFormStatus/UseFormStatus";
 import UseActionState from "./components/useActionState/UseActionState";
+import UseOptimistic from "./components/useOptimistic/UseOptimistic";
 
 function App() {
   return (
     // <Use />
     // <Action />
     // <UseFormStatus />
-    <UseActionState />
+    // <UseActionState />
+    <UseOptimistic />
   );
 }
 
--- frontend/react-notes/app12-react19/src/components/useOptimistic/UseOptimistic.jsx
+++ frontend/react-notes/app12-react19/src/components/useOptimistic/UseOptimistic.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useOptimistic, useState } from "react";
 
+export function submitTitle(formData) {
+  return new Promise((resolve, reject) => {
+    const updatedTitle = formData.get("title");
+    setTimeout(() => {
+      const n = Math.random();
+      if (n < 0.75) {
+        resolve(updatedTitle);
+      } else reject("Error");
+    }, 5000);
+  });
+}
+
+// useOptimistic
+// 서버 요청이 처리되는 동안 예상되는 결과를 미리 반영하여,
+// 사용자가 보다 매끄럽게 애플리케이션을 사용할 수 있도록 도움.
 const UseOptimistic = () => {
-  return <div>UseOptimistic</div>;
+  const [title, setTitle] = useState("title");
+  const [optimisticTitle, setOptimisticTitle] = useOptimistic(title);
+  const [error, setError] = useState(null);
+  const pending = title !== optimisticTitle;
+
+  const handleSubmit = async (formData) => {
+    setError(null);
+    setOptimisticTitle(formData.get("title"));
+    try {
+      const updatedTitle = await submitTitle(formData);
+      setTitle(updatedTitle);
+    } catch (e) {
+      setError(e);
+    }
+  };
+  return (
+    <div>
+      <h2>{optimisticTitle}</h2>
+      <p>{pending && "Updaing..."}</p>
+      <form action={handleSubmit}>
+        <input type="text" name="title" />
+        <button type="submit" disabled={pending}>
+          Submit
+        </button>
+      </form>
+      <div>{error && error}</div>
+    </div>
+  );
 };
 
 export default UseOptimistic;
